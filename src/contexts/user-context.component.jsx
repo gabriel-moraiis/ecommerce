4e32efd33762207526494fa8f0cb9784e80747b8
Gabriel Morais <gabriel.moraiis95@gmail.com>
2022-11-22 15:25:43 -0300

    Substituição de useState por useReducer no contexto de Usuario

--- src/contexts/user-context.component.jsx
+++ src/contexts/user-context.component.jsx
@@ -1,10 +1,38 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 import { createUserDocumentFromAuth, onAuthStateChangedListener } from '../utils/firebaseApp';
 
 export const UserContext = createContext();
 
+const USER_ACTIONS_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER',
+};
+
+const INITIAL_STATE = {
+    currentUser: null,
+};
+
+const userReducer = (state, action) => {
+    console.log(action);
+    const { type, payload } = action;
+
+    switch (type) {
+        case USER_ACTIONS_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload,
+            };
+        default:
+        throw new Error(`Unhandled type ${type} in userReducer`);
+    }
+};
+
 export function UserProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTIONS_TYPES.SET_CURRENT_USER, payload: user });
+  };
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
